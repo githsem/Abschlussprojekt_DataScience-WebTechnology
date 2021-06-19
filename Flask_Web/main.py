34a6314aebf73dbd92d20a0f877c5c3c3e4b5ab5
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-06-20 00:26:45 +0200

    main added

--- Flask_Web/main.py
+++ Flask_Web/main.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    nummer = 10
+    return render_template("index.html",nummer=nummer)
+
+@app.route("/about")
+def about():
+    return "About"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
