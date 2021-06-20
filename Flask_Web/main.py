3933d8f257ccba9af968bfd6a0e06620c044aa8b
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-06-21 01:50:26 +0200

    update

--- Flask_Web/main.py
+++ Flask_Web/main.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import TeamITea
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    result = TeamITea.vorhersage('schlect')
-    return render_template("index.html",result=result)
+    return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "About"
+@app.route('/vorhersage')
+def my_form():
+    return render_template('vorhersage.html')
 
+@app.route('/vorhersage', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    result = TeamITea.vorhersage(text) 
+    return render_template('vorhersage.html',result=result[0])
+
+@app.route("/uberuns")
+def uberuns():
+    return render_template("uberuns.html")   
+   
+  
 
 if __name__ == "__main__":
     app.run(debug=True)
