95e7a90d115bad1df7322b9dba5d554d91c0df28
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-06-23 23:14:28 +0200

    update

---
+++ Flask_WebApp/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import TeamITea
+
+app = Flask(__name__)
+
+df=pd.read_csv("Flask_Web/GM_Clean.csv")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/vorhersage')
+def my_form():
+    return render_template('vorhersage.html')
+
+@app.route('/vorhersage', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    result = TeamITea.vorhersage(text) 
+    return render_template('vorhersage.html',result=result[0],text1=text)
+
+@app.route("/bewertungen")
+def bewerungen():
+    return render_template("bewertungen.html")   
+   
+@app.route("/googlemaps")
+def googlemaps():
+    liste = df['Bewertung']
+    return render_template("googlemaps.html",liste=liste)      
+  
+
+if __name__ == "__main__":
+    app.run(debug=True)
