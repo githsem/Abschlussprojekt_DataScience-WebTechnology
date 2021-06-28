0b583f687602873b7ebbe2f683ae566e45b6109a
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-06-28 02:53:08 +0200

    update

--- Flask_WebApp/main.py
+++ Flask_WebApp/main.py
@@ -9,6 +9,8 @@ kb_stars = df[['NameKlinik', 'Gesamt', 'Zufriedenheit']]
 kb_means = kb_stars.groupby(['NameKlinik'], as_index=False)['Gesamt'].mean()
 kb_means = kb_means.sort_values('Gesamt',ascending=False, ignore_index=True)
 vergleich_list = kb_means['Gesamt'].to_list()
+nameKlinik = kb_means['NameKlinik'].to_list()
+
 
 
 
@@ -28,14 +30,30 @@ def my_form_post():
     return render_template('vorhersage.html',result=result[0],text1=text)
 
 @app.route("/bewertungen")
-def bewerungen():
+def bewertungen():
     dizi = df.groupby(['NameKlinik']).mean().get('Gesamt')
-    return render_template("bewertungen.html",dizi=dizi)   
-   
-@app.route("/googlemaps")
-def googlemaps():
-    liste = df['Bewertung']
-    return render_template("googlemaps.html",liste=liste)      
+    return render_template("bewertungen.html",dizi=dizi,nameKlinik=nameKlinik,display='none')   
+
+@app.route("/bewertungen", methods=['POST'])
+def bewertungen_post():
+    kname = request.form['options']
+
+    # KB Bewertungen
+    df2 = df.where(df["NameKlinik"]==text)
+    df2.dropna(inplace=True)
+    kb_bewertungen = df2['Erfahrungsbericht']
+
+    # KB Sterne
+    kb_sterne = df2['Gesamt']
+    kb_sterne =[int(i) for i in kb_sterne]
+
+    # KB poz_neg result
+    kb_result = df2['Zufriedenheit']
+
+    kb = zip(kb_bewertungen,kb_sterne,kb_result)
+    
+  
+    return render_template("bewertungen.html",kname=kname,display='block',display2='none',nameKlinik=nameKlinik,kb=kb)      
   
 
 if __name__ == "__main__":
