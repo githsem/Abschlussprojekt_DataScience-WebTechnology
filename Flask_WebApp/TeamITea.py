25600061554b78e1a0c748473e559cfc2862e469
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-07-03 23:12:07 +0200

    update

--- Flask_WebApp/TeamITea.py
+++ Flask_WebApp/TeamITea.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-import random
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
